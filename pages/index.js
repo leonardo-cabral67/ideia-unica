9102e8b34d8612be0da6839994a60efc1fca1007
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2023-03-02 03:42:57 +0000

    fix: deleted counter

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,7 @@
 import { useState } from "react"
 
 function home() {
-    return (
-    <div>
-        <h1>Home</h1>
-        <Counter/>
-    </div>
-    )
-}
-
-function Counter() {
-    const [counter, setCounter] = useState(1)
-    const increaseCounter = () => setCounter(counter + 1)
-    const decreaseCounter = () => setCounter(counter - 1)
-
-    return (
-        <div>
-            <h2>Counter: {counter}</h2>
-            <button onClick={increaseCounter}>+</button>
-            <button onClick={decreaseCounter}>-</button>
-        </div>
-    )
+    return <h1>Home</h1>
 }
 
 export default home
