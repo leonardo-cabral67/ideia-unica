7ddccdf56a3e50e29930cad1cbd0fd6b68adc094
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2023-03-04 19:05:40 +0000

    feat: add cache-control header stale while revalidate

--- pages/api/time.js
+++ pages/api/time.js
@@ -1,6 +1,8 @@
 function time(request, response) {
     const dynamicDate = new Date()
 
+    response.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate")
+    
     response.json({
         date: dynamicDate.toGMTString()
     })
